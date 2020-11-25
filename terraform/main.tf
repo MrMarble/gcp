127254dd6d49b7fd62e1b7d219a6cf79a75d111a
Alvaro Tinoco <alvarotinocomarmol@gmail.com>
2020-11-25 14:00:45 +0100

    required providers

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,4 +3,11 @@ terraform {
     bucket = "terraform-backend-19e2dc550026"
     prefix = "terraform/state"
   }
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 3.49.0"
+    }
+  }
 }
